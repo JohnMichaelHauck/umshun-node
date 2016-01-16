b5ebe0cc7b84310e83f7a0c9cb00059bd663c8ba
JohnMichaelHauck <johnmichaelhauck@yahoo.com>
2016-01-16 16:55:09 -0500

    A day's worth of featured added.

--- app.js
+++ app.js
@@ -15,52 +15,84 @@ app.get('/', function (req, res) {
 
 var allSockets = [];
 var nextUid = 1;
-var throttle = 2000;
+var throttleMs = 2000;
 
 sio.on('connection', function (socket) {
-    
+
+    allSockets.push(socket);
+
     socket.myUid = nextUid++;
     socket.myScore = 0;
     socket.myWord = '';
-    socket.myTod = new Date().getTime() - throttle;
-    
-    allSockets.push(socket);
-    console.log('connected ' + socket.myUid + ', players: ' + allSockets.length);
+    socket.myTod = new Date().getTime() - throttleMs;
+    socket.myName = 'player ' + socket.myUid;
+    socket.mySpeaker = 'default';
+    socket.join(socket.mySpeaker);
 
-    socket.on('heard', function (word) {
+    console.log('player ' + socket.myUid + ': connected');
+    console.log('players: ' + allSockets.length);
+
+    socket.on('name', function (name) { // player tells us their name
+        console.log('player ' + socket.myUid + ': name = ' + name);
+        socket.myName = name;
+    });
+
+    socket.on('speaker', function (speaker) { // player is listening to a specific speaker
+        speaker = speaker.toLowerCase();
+        console.log('player ' + socket.myUid + ': speaker = ' + speaker);
+        socket.leave(socket.mySpeaker);
+        socket.mySpeaker = speaker;
+        socket.join(socket.mySpeaker);
+    });
+
+    socket.on('heard', function (word) { // player heard a word
+        word = word.toLowerCase();
         var tod = new Date().getTime();
-        if (socket.myTod + throttle > tod) {
-            console.log('spam from ' + socket.myUid);
+        if (socket.myTod + throttleMs > tod) { // limit player to one heard every 2 seconds
+            console.log('player ' + socket.myUid + ': throttled = ' + word);
         }
         else {
-            console.log('heard ' + word + ' from ' + socket.myUid);
-            for (var i = 0; i < allSockets.length; i++) {
+            console.log('player ' + socket.myUid + ': heard = ' + word);
+            for (var i = 0; i < allSockets.length; i++) { // other players score from this player ...
                 var other = allSockets[i];
-                if (other != socket) {
-                    if (other.myWord == word) {
-                        if (other.myTod + throttle > tod) {
-                            other.myScore++;
-                            console.log('  player ' + other.myUid + ' gets a point');
+                if (other.mySpeaker === socket.mySpeaker) { // .. who are listening to the same speaker ...
+                    if (other.myWord === word) { // ... and just played the same word ...
+                        if (other.myTod + throttleMs > tod) { // ... less than 2 seconds ago.
+                            other.myScore++; // logically this cannot be the same as the player (2 second rule)
+                            console.log('  player ' + other.myUid + ': scores');
                         }
                     }
                 }
             }
-            socket.myTod = tod;
+            socket.myTod = tod; // save so this player can score from others who play this word in the next two seconds
             socket.myWord = word;
         }
     });
-    
-    socket.on('score', function () {
-       socket.emit('score', socket.myScore);
-       console.log('player ' + socket.myUid + ', score: ' + socket.myScore); 
+
+    socket.on('score', function () { // player wants to know their score
+        console.log('player ' + socket.myUid + ': score = ' + socket.myScore);
+        socket.emit('score', socket.myScore);
     });
 
-    socket.on('disconnect', function () {
+    socket.on('scores', function (n) { // player wants to know the top n scores
+        console.log('player ' + socket.myUid + ': scores = ' + n);
+
+        var players = allSockets
+            .map(function (s) { return { name: s.myName, score: s.myScore, speaker: s.mySpeaker }; })
+            .filter(function (a) { return a.speaker === socket.mySpeaker; })
+            .sort(function (a, b) { return b.score - a.score; })
+            .slice(0, n - 1);
+
+        socket.emit('scores', players);
+    });
+
+    socket.on('disconnect', function () { // player is bored
         allSockets.splice(allSockets.indexOf(socket), 1);
-        console.log('disconnected ' + socket.myUid + ', players: ' + allSockets.length);
+        console.log('player ' + socket.myUid + ': disconnected');
+        console.log('players: ' + allSockets.length);
     });
 });
 
 server.listen(3000, function () {
-    console.log('Server');
+    console.log('Server on port 3000');
 });
--- public/index.js
+++ public/index.js
@@ -1,20 +1,56 @@
 var socket = io();
 var intervalId = 0;
+var words;
 
-function myclick(word) {
+$(document).ready(function () {
+    for (var i = 0; i < 9; i++) {
+        $("#word-buttons").append("<button id='word" + i + "' class='btn btn-primary' onclick='heardWord(" + i + ");'>...</button>");
+    };
 
+    for (var i = 0; i < 9; i++) {
+        $("#scores").append("<li class='list-group-item'><b>" + i + ":&nbsp</b><text id='player" + i + "'>...</text><span id='score" + i + "' class='badge'>0</span></li>");
+    }
+
+    wordsChanged();
+});
+
+function nameChanged() {
+    socket.emit("name", $('#name')[0].value);
+};
+
+function speakerChanged() {
+    socket.emit("speaker", $('#speaker')[0].value);
+};
+
+function wordsChanged() {
+    words = $('#words')[0].value.split(",");
+    for (var i = 0; i < 9; i++) {
+        $("#word" + i).text(words[i]);
+    };
+};
+
+function heardWord(index) {
+    var word = words[index];
     if (intervalId == 0) {
-        
-        socket.emit('heard', word);
-
+        socket.emit("heard", word);
         intervalId = setInterval(function () {
-            socket.emit('score');
+            socket.emit("score");
+            socket.emit("scores", 9);
             clearInterval(intervalId);
             intervalId = 0;
         }, 2000);
     }
 }
 
-socket.on('score', function(score) {
-     $('#score').text(score);
+socket.on("score", function (score) {
+    $("#score").text(score);
+});
+
+socket.on("scores", function (players) {
+    var i = 0;
+    players.forEach(function (player) {
+        $("#player" + i).text(player.name);
+        $("#score" + i).text(player.score);
+        i++;
+    });
 });
